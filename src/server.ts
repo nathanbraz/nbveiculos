0938a2d656d3dfb87da2875af528bee2148f2630
Nathan Braz <nathanbrz09@gmail.com>
2021-09-06 23:46:47 -0300

    Removendo linha

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes'; './routes/categories.routes';
-
 const app = express();
 
 app.use(express.json());
-
 
 
 app.listen(3333, () => console.log("Server iniciado"));
