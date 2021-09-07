88c48b0d200a9c3e7afe4ab44459c7144622c4ea
Nathan Braz <nathanbrz09@gmail.com>
2021-09-07 00:39:58 -0300

    Criando o repositorio de categoria e adicionando metodos de listagem e findby

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,30 @@
 import { Router } from "express";
 import { Category } from "../model/Category";
+import { CategoryRepository } from "../respositories/CategoryRepository";
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  const categoryAlreadyExists = categoryRepository.findByname(name);
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  })
+  if(categoryAlreadyExists){
+    throw response.status(400).json({ message: "Category name already exists!" });
+  }
 
-  categories.push(category);
-
-  return response.status(201).json(categories);
+  categoryRepository.create({name, description});
+  
+  return response.status(201).send();
 });
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoryRepository.list();
+
+  return response.json(all);
+});
+
 
 export { categoriesRoutes };
