17d949b930b8d62600fac8e547de69946332bfa3
Nathan Braz <nathanbrz09@gmail.com>
2021-09-06 22:25:42 -0300

    Instalando o ts-node-dev e organizando os scrips de inicializaçao

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,4 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World"});
-})
-
-app.listen(3333, () => console.log("Server inciado"));
+app.listen(3333, () => console.log("Server iniciado"));
