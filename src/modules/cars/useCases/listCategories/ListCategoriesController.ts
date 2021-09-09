60de951000869deca7d9eac128622a127838ee61
Nathan Braz <nathanbrz09@gmail.com>
2021-09-08 22:38:31 -0300

    Criando as useCases (servicos) e controllers para cadastro e listagem de categorias

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { Category } from "../../model/Category";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+
+class ListCategoriesController {
+
+  constructor(private listCategoriesUseCase: ListCategoriesUseCase) { }
+
+  handle(request: Request, response: Response): Response {
+    const categories = this.listCategoriesUseCase.execute();
+
+    return response.json(categories);
+  }
+}
+
+export { ListCategoriesController };
