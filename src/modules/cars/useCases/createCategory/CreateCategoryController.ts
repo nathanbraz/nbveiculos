3810d61d1741d27eff0d7f4867bc3e3d71a18bbd
Nathan Braz <nathanbrz09@gmail.com>
2021-09-30 22:47:12 -0300

    Fazendo o primeiro teste de exemplo com soma de numeros

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -3,7 +3,6 @@ import { container } from "tsyringe";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoriesController {
-
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
   
